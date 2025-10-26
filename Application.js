42bcf5d463287fda96af35b6aa368c794f7c07a8
harsh0340 <84018832+harsh0340@users.noreply.github.com>
2025-10-26 18:13:34 +0530

    Update Application.js

--- Application.js
+++ Application.js
@@ -1,11 +1,1 @@
-import mongoose from "mongoose";
 
-const applicationSchema = new mongoose.Schema({
-  jobId: String,
-  name: String,
-  mobile: String,
-  city: String,
-  appliedAt: { type: Date, default: Date.now }
-});
-
-export default mongoose.model("Application", applicationSchema);
